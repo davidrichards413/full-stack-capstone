e62437aeb747169be7f9e9b708003c2fbcea19c7
AbisolaOseni <abisola.oseni@gmail.com>
2024-11-05 08:50:15 -0800

    now gets live article list from server

---
+++ front-end/src/NewsReader.js
@@ -0,0 +1,63 @@
+import { QueryForm } from "./QueryForm";
+import { Articles } from "./Articles";
+import { useState, useEffect } from "react";
+import { exampleQuery, exampleData } from "./data";
+
+export function NewsReader() {
+  const [query, setQuery] = useState(exampleQuery); // latest query send to newsapi
+  const [data, setData] = useState(exampleData); // current data returned from newsapi
+  const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
+
+  const urlNews = "/news";
+
+  useEffect(() => {
+    getNews(query);
+  }, [query]);
+
+  function onFormSubmit(queryObject) {
+    setQuery(queryObject);
+  }
+
+  async function getNews(queryObject) {
+    if (queryObject.q) {
+      try {
+        const response = await fetch(urlNews, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(queryObject),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    } else {
+      setData({});
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <section className="parent">
+          <div className="box">
+            <span className="title">Query Form</span>
+            <QueryForm
+              setFormObject={setQueryFormObject}
+              formObject={queryFormObject}
+              submitToParent={onFormSubmit}
+            />
+          </div>
+          <div className="box">
+            <span className="title">Articles List</span>
+            <Articles query={query} data={data} />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
