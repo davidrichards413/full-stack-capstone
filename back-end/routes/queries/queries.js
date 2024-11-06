51aa4c883831bd3f996d86b212fdffb382f79aad
Dave Richards <davidrichards413@gmail.com>
2024-11-06 16:51:12 -0500

    queries are read from and written to MongoDB

--- back-end/routes/queries/queries.js
+++ back-end/routes/queries/queries.js
@@ -2,22 +2,50 @@ import fs from "fs";
 import express from "express";
 const queriesRouter = express.Router();
 
-queriesRouter.get("/", (req, res) => {
+import { client, connect } from "../../db.js";
+const dbName = "queriesdb";
+const collectionName = "queries";
+connect(); // Connect to MongoDB
+
+queriesRouter.get("/", async (req, res) => {
   try {
-    const data = fs.readFileSync("queries.json", "utf8");
-    res.status(200).send(data);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const queries = await collection.find({}).toArray();
+    console.log(queries);
+    res.status(200).json(queries);
   } catch (err) {
     if (res.status) console.error(err);
-    res.status(404).send("queries.json file not found");
+    res.status(500).send("queries table not read");
   }
 });
 
-queriesRouter.post("/", (req, res) => {
-  const queryArray = req.body;
-  const data = JSON.stringify(queryArray, null, 2);
+queriesRouter.get("/user/:user", async (req, res) => {
   try {
-    fs.writeFileSync("queries.json", data);
-    console.log("query array saved to queries.json");
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const filter = { user: req.params.user };
+    const queries = await collection.find(filter).toArray();
+    // console.log(queries);
+    // console.log(queries.length);
+    if (queries.length > 0) {
+      res.status(200).json(queries);
+    } else {
+      res.status(404).send(`no queries found for ${req.params.user}`);
+    }
+  } catch (err) {
+    if (res.status) console.error(err);
+    res.status(500).send("queries table not read");
+  }
+});
+
+queriesRouter.post("/", async (req, res) => {
+  const queryArray = req.body;
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    await collection.insertOne(queryArray);
+    console.log("query array saved to MongoDB");
     res.status(200).send("query array saved");
   } catch (err) {
     console.error(err);
